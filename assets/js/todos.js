78551ed304530061bf31bfe05cb45649c8c79f72
bipana50 <bipanashrestha50@gmail.com>
2019-11-12 16:30:11 +0545

    all commited

---
+++ assets/js/todos.js
@@ -0,0 +1,36 @@
+$("ul").on("click","li",function(){
+	//if($(this).css("color")==="rgb(128, 128, 128)"){
+	//	$(this).css({
+	//	color: "black",
+	//	textDecoration: "none"
+	//});
+	//}
+	//else{
+	//$(this).css({
+	//	color: "gray",
+	//	textDecoration: "line-through"
+	//});
+    //    }
+
+    $(this).toggleClass("completed");
+});
+$("ul").on("click","span",function(event){
+	
+	
+	$(this).parent().fadeOut(500,function(){
+       $(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+ if(event.which === 13){
+  var todotext= $(this).val();
+  $(this).val("");
+  $("ul").append("<li><span><i class='far fa-trash-alt'></i> </span> "+ todotext + "</li>");
+ }
+});
+$(".fa-plus").click(function() {
+	// body...
+$("input[type='text']").fadeToggle();
+});
